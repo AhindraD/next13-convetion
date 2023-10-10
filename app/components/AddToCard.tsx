2f7e0b6e2abb9a74d6c2d38e2e93a33792235350
Ahindra D <official.ahindra@gmail.com>
2023-10-10 20:02:29 +0530

    client

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const AddToCard = () => {
+    return (
+        <div>
+            <button onClick={() => { console.log('add to cart') }}>add to cart</button>
+        </div>
+    )
+}
+
+export default AddToCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
       <h1>Hi Mom & Dad</h1>
       <Link href="/user">user-page</Link>
       <br />
-      <a href="/user/new">user-page-new</a>
+      <ProductCard />
     </main>
   )
 }
