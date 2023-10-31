1a347e0fb3be250cc9263a9a56c57dfda08f35c2
Ahindra D <official.ahindra@gmail.com>
2023-10-31 17:35:02 +0530

    twailwind

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AddToCard from './AddToCard';
-import styles from './ProductCard.module.css';
+
+//to use css from module
+// import styles from './ProductCard.module.css';
+// className={styles.card}
 
 const ProductCard = () => {
     return (
-        <div className={styles.card}>
+        <div
+            className="p-4 my-3 bg-cyan-300 text-white text-lg font-semibold hover:bg-green-300 w-40 text-center">
             <AddToCard />
         </div>
     )
