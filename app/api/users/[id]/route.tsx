3bb1d43c87276d0e4a07abdb597f4bf851ea691e
Ahindra D <official.ahindra@gmail.com>
2023-11-13 05:10:42 +0530

    PutReq

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -6,10 +6,26 @@ interface Props {
     }
 }
 export function GET(request: NextRequest, { params: { id } }: Props) {
+    //simulate user doesn't exist
     if (id > 9) {
         return NextResponse.json({ "message": "user out of scope" }, { status: 404 })
     }
+
     return NextResponse.json({
         id, name: 'Eebbe'
     }, { status: 200 })
+}
+
+//PUT  -->  update object
+//PATCH  -->  partial update/ some properties update
+export async function PUT(request: NextRequest, { params: { id } }: Props) {
+    const body = await request.json();
+    if (!body.name) {
+        return NextResponse.json({ message: "Name is required" }, { status: 400 })
+    }
+    //simulate user doesn't exist
+    if (id > 9) {
+        return NextResponse.json({ message: "user out of scope" }, { status: 404 })
+    }
+    return NextResponse.json({ message: "Success", id, name: body.name }, { status: 201 })
 }
