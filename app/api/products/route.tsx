db25b8af82776962701c4719f3ad1476b9df457e
Ahindra D <official.ahindra@gmail.com>
2023-12-06 00:12:15 +0530

    productDB

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,26 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import productSchema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request:NextRequest){
-    return NextResponse.json([
-        {
-            id:1,
-            name:"Protein",
-            price:35
-        },
-        {
-            id:2,
-            name:"Citrulline",
-            price:15
-        }
-    ],{status:200})
+export async function GET(request: NextRequest) {
+    try {
+        const products = await prisma.product.findMany();
+        return NextResponse.json(products, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 })
+    }
 }
 
-export async function POST(request:NextRequest){
-    const body=await request.json();
-    const validation=productSchema.safeParse(body);
-    if(!validation.success){
-        return NextResponse.json({error:validation.error.errors},{status:400})
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = productSchema.safeParse(body);
+    if (!validation.success) {
+        return NextResponse.json({ error: validation.error.errors }, { status: 400 })
     }
-    return NextResponse.json({message:"Success",id:11,name:body.name,price:body.price},{status:201})
+    try {
+        const product = await prisma.product.create({
+            data: {
+                name: body.name,
+                price: body.price
+            }
+        })
+        return NextResponse.json({ message: "Success", id: 11, name: body.name, price: body.price }, { status: 201 })
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 })
+    }
 }
