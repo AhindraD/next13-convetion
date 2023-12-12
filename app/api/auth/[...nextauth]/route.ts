3c70eb28fa7c835bb65ec4890e1a9816dea3dd5f
Ahindra D <official.ahindra@gmail.com>
2023-12-12 06:29:44 +0530

    DatabaseAdapters

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,14 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
+import { NextAuthOptions } from "next-auth";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),//to save session in database
+    session: {
+        strategy: "jwt" //when using adapter, it defaults to database <-- which is not supported in OAuth, so we have to explicitly set it to jwt
+    },
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
